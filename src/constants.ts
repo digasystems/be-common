22feaad441c720799abefbc32cea56e5459f90e4
Angrigo <gab.angrisani@gmail.com>
2021-11-23 15:31:48 +0100

    remove auth keys

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,8 @@ import path from "path";
 import fs from "fs";
 
 const constants = {
-    privateKEY: fs.readFileSync(path.join(__dirname, './keys/jwtRS256.key'), 'utf8'),
-    publicKEY: fs.readFileSync(path.join(__dirname, './keys/jwtRS256.key.pub'), 'utf8')
+    // privateKEY: fs.readFileSync(path.join(process.cwd(), './keys/jwtRS256.key'), 'utf8'),
+    // publicKEY: fs.readFileSync(path.join(process.cwd(), './keys/jwtRS256.key.pub'), 'utf8')
 }
 
 export default constants;
